d392d6f20ebe6ec3c8e6f9249537fc862c2df1ba
Andy Lo <andylolu24@gmail.com>
2023-11-04 22:00:43 +0000

    Frontend testing

--- DiagLLMFrontend/src/global.ts
+++ DiagLLMFrontend/src/global.ts
@@ -1,8 +1,5 @@
-import axios from "axios";
-
-export async function write_to_log(message: string)
-{
-    await axios.post("http://localhost:5000/log", {
-        message: message,
-    })
+export async function write_to_log(message: string) {
+    // await axios.post("http://localhost:5000/log", {
+    //     message: message,
+    // })
 }
